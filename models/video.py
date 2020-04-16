e45d94bc73bc97515d38edc097c89c6a884f90b3
Arseniy <ylarseniy@gmail.com>
2020-04-16 15:54:59 +0500

    add query parsing

--- models/video.py
+++ models/video.py
@@ -7,7 +7,7 @@ from googleapiclient.errors import HttpError
 
 from settings import YOUTUBE_API_KEY
 
-
+from pprint import pprint
 ytdl_format_options = {
     'format': 'bestaudio/best',
     'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
@@ -36,6 +36,7 @@ def youtube_search(query: str):
     ).execute()
 
     for search_result in search_response.get('items', []):
+        pprint(search_result)
         return search_result['id']['videoId']
 
 
@@ -44,16 +45,16 @@ class Video:
         self.url = url
         self.title = title
 
-    async def get_music_url(self):
+    async def get_music_info(self):
         if self.url is not None:
             loop = asyncio.get_event_loop()
             data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url=self.url, download=False))
-            return data['url']
+            return data
         elif self.title is not None:
-            url = youtube_search(self.title)
-            if not url:
+            video_id = youtube_search(self.title)
+            if not video_id:
                 return
             loop = asyncio.get_event_loop()
             data = await loop.run_in_executor(None, lambda: ytdl.extract_info(
-                url="https://www.youtube.com/watch?v=" + url, download=False))
-            return data['url']
+                url="https://www.youtube.com/watch?v=" + video_id, download=False))
+            return data
--- music.py
+++ music.py
@@ -1,8 +1,10 @@
+import re
+
 import discord
 from discord.ext import commands
 
 from models import Video
-
+from pprint import pprint
 ffmpeg_options = {
     'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
     'options': '-vn -nostats -loglevel 0'
@@ -14,11 +16,31 @@ class Music(commands.Cog):
         self.bot = bot
 
     @commands.command('play')
-    async def play(self, context: commands.context.Context, url: str):
-        music_url: str = await Video(url=url).get_music_url()
+    async def play(self, context: commands.context.Context, *args):
+        query = " ".join(args).strip()
+        if self.is_yt_video(query):
+            music_info: dict = await Video(url=query).get_music_info()
+        elif self.is_yt_playlist(query):
+            # TODO: playlist handler
+            pass
+        else:
+            music_info: dict = await Video(title=query).get_music_info()
+
         voice_channel: discord.VoiceChannel = context.guild.voice_channels[0]
         voice_client: discord.voice_client.VoiceClient = await voice_channel.connect()
         # async with context.typing(): зачем-то в примерах есть, но сути не меняет
         # пойму, что и как, тогда удалю, либо оставлю
-        voice_client.play(discord.FFmpegPCMAudio(music_url, **ffmpeg_options))
-        await context.send(f'playing {url}')
+        voice_client.play(discord.FFmpegPCMAudio(music_info['url'], **ffmpeg_options))
+        await context.send(f'playing {music_info["title"]}')
+
+    @staticmethod
+    def is_yt_video(url: str) -> bool:
+        if re.match(r"(https?://)?(www.)?youtu.be|(https?://)?(www.)?youtube.com/watch\?v=", url):
+            return True
+        return False
+
+    @staticmethod
+    def is_yt_playlist(url: str) -> bool:
+        if re.match(r"(https?://)?(www.)?youtube.com/playlist\?", url):
+            return True
+        return False
