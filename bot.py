b76ccc9966b002cd57ae0a189a992ee8ef529b5c
DmitryStO <57713016+DmitryStO@users.noreply.github.com>
2020-04-16 17:38:46 +0500

    Добавил обращение к бд

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 from discord.ext import commands
-
+from data import db_session
 from settings import TOKEN
 from music import Music
+
 
 
 class PeyokaBot(commands.Bot):
@@ -14,6 +15,7 @@ class PeyokaBot(commands.Bot):
 
 
 if __name__ == '__main__':
+    db_session.global_init("db/music.sqlite")
     bot = PeyokaBot()
     bot.add_cog(Music(bot))
     bot.run(TOKEN)
